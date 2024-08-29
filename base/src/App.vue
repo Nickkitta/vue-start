ca6db4e3638d351baad82e5a7711809aaef49205
gurin <nikita.gurin@intervolga.ru>
2024-08-29 22:04:12 +0300

    [main] - add

--- base/src/App.vue
+++ base/src/App.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-
+  <main class="flex flex-col w-9/12">
+    <h1 class="text-blue-500">Tailwind</h1>
+    <h1 class="base-header">SCSS</h1>
+  </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.base-header {
+  $color: #f9f9;
 
+  color: $color;
+  text-align: center;
+}
 </style>
